09be7d0275bc495d891be655984c21d236e1fdea
sa46lll <9812smh@naver.com>
2022-08-23 12:21:29 +0900

    feat: Message List 조회 API 추가

---
+++ src/main/java/com/kurly/projectmaic/domain/center/dao/MessageRepository.java
@@ -0,0 +1,11 @@
+package com.kurly.projectmaic.domain.center.dao;
+
+import com.kurly.projectmaic.domain.center.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+	List<Message> findMessagesByWorkerId(final long workerId);
+}
---
+++ src/main/java/com/kurly/projectmaic/domain/center/domain/Message.java
@@ -0,0 +1,36 @@
+package com.kurly.projectmaic.domain.center.domain;
+
+import com.kurly.projectmaic.domain.center.enumeration.RoundStatus;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Message {
+
+	@Id
+	@Column(name = "message_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long messageId;
+
+	@Column(name = "worker_id")
+	private Long workerId;
+
+	@Column(name = "content")
+	private String content;
+
+	@Column(name = "full_location")
+	private String fullLocation;
+
+	public Message(Long workerId, String content, String fullLocation) {
+		this.workerId = workerId;
+		this.content = content;
+		this.fullLocation = fullLocation;
+	}
+}
